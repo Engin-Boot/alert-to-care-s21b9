e039dd2aab027934ec774bbe73508d917d41fc79
Maurya <Aruna.Maurya@philips.com>
2020-10-22 17:44:44 +0530

    Changed the frontend app to a .net core app

---
+++ AlertToCareFrontend/Views/HomePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+namespace AlertToCareFrontend.Views
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : UserControl
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AlertToCareFrontend/Views/RespondToCare.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AlertToCareFrontend.Views
+{
+    /// <summary>
+    /// Interaction logic for RespondToCare.xaml
+    /// </summary>
+    public partial class RespondToCare : UserControl
+    {
+        public RespondToCare()
+        {
+            InitializeComponent();
+        }
+    }
+}
